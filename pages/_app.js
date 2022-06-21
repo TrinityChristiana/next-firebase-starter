3ff10bdbc9d023dbbd5282258ceeb0f1703393df
Trinity Christiana <trinitycterry@gmail.com>
2022-06-20 22:15:32 -0500

    Added firebase auth to app

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,33 @@
 /* eslint-disable react/prop-types */
+import Loading from '../components/Loading';
+import Signin from '../components/Signin';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/globals.css';
+import { AuthConsumer, AuthProvider } from '../utils/context/authContext';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <AuthProvider>
+      <AuthConsumer>
+        {({ user, userLoading }) => {
+          if (userLoading) {
+            return <Loading />;
+          }
+
+          if (user) {
+            return (
+              <div className="container">
+                <Component {...pageProps} />
+              </div>
+            );
+          }
+
+          return <Signin />;
+        }}
+      </AuthConsumer>
+    </AuthProvider>
+  );
 }
 
 export default MyApp;
